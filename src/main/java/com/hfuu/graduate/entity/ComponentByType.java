983545e04ab536abef6c366149bcf74fa38407a6
deer-li <1597493814@qq.com>
2022-11-28 15:44:04 +0800

    添加表单属性实体和数据表的映射

---
+++ src/main/java/com/hfuu/graduate/entity/ComponentByType.java
@@ -0,0 +1,35 @@
+package com.hfuu.graduate.entity;
+
+import java.util.Date;
+
+public class ComponentByType {
+    private Integer cid;
+
+    private String cname;
+
+    private Date createTime;
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname == null ? null : cname.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
